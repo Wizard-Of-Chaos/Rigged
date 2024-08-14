6df9bdf7d255a1c76521cfc0ace252930eb1608a
Alexander Wiecking <sandosius@gmail.com>
2024-08-14 14:37:41 -0400

    Initial commit of files

---
+++ assets/camera_controller.gd
@@ -0,0 +1,33 @@
+extends Node
+
+@onready var yaw_node = $CamYaw
+@onready var pitch_node = $CamYaw/CamPitch
+@onready var camera = $CamYaw/CamPitch/Camera3D
+
+var yaw : float = 0
+var pitch : float = 0
+
+var yaw_sensitivity : float = 0.07
+var pitch_sensitivity : float = 0.07
+
+var yaw_acceleration : float = 15
+var pitch_acceleration : float = 15
+
+var pitch_max : float = 75
+var pitch_min : float = -55
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		yaw += -event.relative.x * yaw_sensitivity
+		pitch += event.relative.y * pitch_sensitivity
+
+func _physics_process(delta):
+	pitch = clamp(pitch, pitch_min, pitch_max)
+	yaw_node.rotation_degrees.y = lerp(yaw_node.rotation_degrees.y, yaw, yaw_acceleration * delta)
+	pitch_node.rotation_degrees.x = lerp(yaw_node.rotation_degrees.x, pitch, pitch_acceleration * delta)
