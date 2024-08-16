608ce7af726d7c646487091ab058d89c8432c617
Kyle Simon <kmsimon1@outlook.com>
2024-08-16 12:36:37 -0400

    John Rigged can now jump Also App ID acquired yay

--- player.gd
+++ player.gd
@@ -1,15 +1,17 @@
 extends CharacterBody3D
 
-@export var speed = 14
-@export var fall_acceleration = 75
+@export var speed: float = 14
+@export var fall_acceleration: float = 75
+@export var jump_impulse: float = 40
 
-var target_velocity = Vector3.ZERO
+var target_velocity := Vector3.ZERO
 
 func _physics_process(delta: float) -> void:
-	var direction = Vector3.ZERO
+	var direction := Vector3.ZERO
 	var movementData := Steam.getAnalogActionData(SteamGlobal.player_one, SteamGlobal.movementHandle)
-	direction.x = -movementData["x"]
-	direction.z = movementData["y"]
+	var jumpData := Steam.getDigitalActionData(SteamGlobal.player_one, SteamGlobal.jumpHandle)
+	direction.x = -movementData.x
+	direction.z = movementData.y
 	if direction != Vector3.ZERO:
 		direction = direction.normalized()
 	target_velocity.x = direction.x * speed
@@ -17,6 +19,8 @@ func _physics_process(delta: float) -> void:
 	
 	if not is_on_floor():
 		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
+	elif jumpData.state:
+		target_velocity.y = jump_impulse
 	
 	velocity = target_velocity
 	move_and_slide()
--- steam_global.gd
+++ steam_global.gd
@@ -1,6 +1,6 @@
 extends Node
 
-var APP_ID: int = 480  # TODO: replace with real app id once we get it
+var APP_ID: int = 3174650
 
 var is_on_steam_deck: bool = false
 var is_online: bool = false
@@ -13,6 +13,8 @@ var player_one: int = -1
 var inGameControlsHandle: int = -1
 var movementHandle: int = -1
 var cameraHandle: int = -1
+var menuControlsHandle: int = -1
+var jumpHandle: int = -1
 
 func _ready() -> void:
 	initialize_steam()
@@ -24,9 +26,6 @@ func _process(_delta: float) -> void:
 	if player_one == -1 and controllers.size() > 0:
 		player_one = controllers[0]
 		Steam.activateActionSet(player_one, inGameControlsHandle)
-	
-	
-	var menuControlsHandle := Steam.getActionSetHandle("MenuControls")
 
 func initialize_steam() -> void:
 	var initialize_response: Dictionary = Steam.steamInitEx(true, APP_ID)
@@ -46,5 +45,7 @@ func initialize_steam() -> void:
 	var inputInitialized := Steam.inputInit()
 	Steam.runFrame()
 	inGameControlsHandle = Steam.getActionSetHandle("InGameControls")
+	menuControlsHandle = Steam.getActionSetHandle("MenuControls")
 	movementHandle = Steam.getAnalogActionHandle("Move")
 	cameraHandle = Steam.getAnalogActionHandle("Camera")
+	jumpHandle = Steam.getDigitalActionHandle("Jump")
