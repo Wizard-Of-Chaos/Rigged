5f26a46872c85b9371c6ed3a05f77de192883ad7
Kyle Simon <kmsimon1@outlook.com>
2024-08-17 11:35:26 -0400

    Actually go the direction the camera is pointing

--- player.gd
+++ player.gd
@@ -4,6 +4,9 @@ extends CharacterBody3D
 @export var fall_acceleration: float = 75
 @export var jump_impulse: float = 40
 
+@onready var camera_yaw: Node3D = $CamRig/CameraRoot/CamYaw
+@onready var camera_root: Node3D = $CamRig/CameraRoot
+
 var target_velocity := Vector3.ZERO
 
 func _physics_process(delta: float) -> void:
@@ -19,6 +22,10 @@ func _physics_process(delta: float) -> void:
 	var jump := Input.is_action_pressed("jump")
 	if direction.length_squared() > 1:
 		direction = direction.normalized()
+	basis = camera_yaw.basis
+	if not basis.z.is_equal_approx(Vector3.MODEL_FRONT):
+		var axis := Vector3.MODEL_FRONT.cross(basis.z).normalized()
+		direction = direction.rotated(axis, Vector3.MODEL_FRONT.angle_to(basis.z))
 	
 	target_velocity.x = direction.x * speed
 	target_velocity.z = direction.z * speed
@@ -29,4 +36,7 @@ func _physics_process(delta: float) -> void:
 		target_velocity.y = jump_impulse
 	
 	velocity = target_velocity
+	if target_velocity.is_zero_approx():
+		return
 	move_and_slide()
+	camera_root.position = position + Vector3(0, 5.259, 0)
