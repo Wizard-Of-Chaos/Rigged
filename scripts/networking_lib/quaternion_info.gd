d9dd1a9a8d067972064aa84b71da2bda5465ef5d
Kyle Simon <kmsimon1@outlook.com>
2024-08-27 19:47:53 -0400

    moved over my serialization code

---
+++ scripts/networking_lib/quaternion_info.gd
@@ -0,0 +1,8 @@
+class_name QuaternionInfo 
+extends SerializationInfo
+
+@export var float_info: FloatInfo
+
+func _init(p_name: String = "", p_info: FloatInfo = null, p_use_smallest_three: bool = false):
+	super(name)
+	self.float_info = p_info
---
+++ scripts/networking_lib/serialization_info.gd
@@ -0,0 +1,7 @@
+class_name SerializationInfo
+extends Resource
+
+@export var name: String
+
+func _init(name: String = ""):
+	self.name = name
