60d59c1d8bd2a1428937f554c269e2e587188981
Alexander Wiecking <sandosius@gmail.com>
2024-09-24 13:42:03 -0400

    removed collision shape from shipcell

--- scenes/environments/ship_cell.gd
+++ scenes/environments/ship_cell.gd
@@ -6,6 +6,14 @@ class_name ShipCell
 @export var cells_z = 1
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	
+	var collision_shape: CollisionShape3D = CollisionShape3D.new()
+	var box = BoxShape3D.new()
+	box.size = Vector3(cells_x * 32, cells_y * 16, cells_z * 32)
+	collision_shape.position = Vector3(0, box.size.y * 8, 0)
+	collision_shape.shape = box
+	self.add_child(collision_shape)
+	
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
