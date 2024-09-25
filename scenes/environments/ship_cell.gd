90bbdfb6e975bd539277605f73c5501098e44b1e
Alexander Wiecking <sandosius@gmail.com>
2024-09-24 23:00:03 -0400

    forgor to add the logic to ship skeleton.gd

--- scenes/environments/ship_cell.gd
+++ scenes/environments/ship_cell.gd
@@ -4,7 +4,7 @@ class_name ShipCell
 @export var cells_x = 1
 @export var cells_y = 1
 @export var cells_z = 1
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
 	
 	var collision_shape: CollisionShape3D = CollisionShape3D.new()
@@ -13,9 +13,6 @@ func _ready():
 	collision_shape.position = Vector3(0, box.size.y * 8, 0)
 	collision_shape.shape = box
 	self.add_child(collision_shape)
-	
-	pass # Replace with function body.
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
--- scenes/environments/ship_skeleton.gd
+++ scenes/environments/ship_skeleton.gd
@@ -14,13 +14,16 @@ class_name ShipSkeleton
 @export var oxygen_position: Vector3 = Vector3(0,0,0)
 @export var airlock_positions: Dictionary
  
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	var collision_shape: CollisionShape3D = CollisionShape3D.new()
+	var box = BoxShape3D.new()
+	box.size = Vector3(cells_wide * 32, cells_tall * 16, cells_long * 32)
+	collision_shape.position = Vector3(0, box.size.y * 8, 0)
+	collision_shape.shape = box
+	self.add_child(collision_shape)
+	
 func generate():
 	pass
 	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
