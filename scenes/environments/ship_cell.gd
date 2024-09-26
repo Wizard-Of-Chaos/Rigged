0779051eabf2312b0b0f8ab7f13e05d4127f9ea8
Alexander Wiecking <sandosius@gmail.com>
2024-09-26 14:14:18 -0400

    added two doors to the bridge scene

--- scenes/environments/ship_cell.gd
+++ scenes/environments/ship_cell.gd
@@ -5,6 +5,13 @@ class_name ShipCell
 @export_range(1, 6) var cells_y = 1
 @export_range(1, 6) var cells_z = 1
 
+#If you put anything other than vectors in this I'll fucking kill you
+@export var entry_positions: Dictionary
+enum ENTRY_SIZE {SMALL, LARGE, VENT}
+@export var entry_position_sizes: Dictionary
+enum ENTRY_POS_LOCATION {UP, DOWN, LEFT, RIGHT, FORWARD, BACK}
+@export var entry_position_type: Dictionary
+
 func _ready():
 	
 	var collision_shape: CollisionShape3D = CollisionShape3D.new()
