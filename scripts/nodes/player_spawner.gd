86e97280638805dd1cc10583ddd3e30206430ef0
Alexander Wiecking <sandosius@gmail.com>
2024-12-03 15:32:41 -0500

    widened hallways, added junk to bridge

--- scripts/nodes/player_spawner.gd
+++ scripts/nodes/player_spawner.gd
@@ -15,7 +15,7 @@ func spawn_player(data: Dictionary) -> Node:
 	var player_scene = preload("res://scenes/actors/player.tscn")
 	var player_instance = player_scene.instantiate()
 	player_instance.name = "Player%s" % data.peer_id
-	player_instance.position.x = randi_range(-20, 20)
+	player_instance.position.x = randi_range(-10, 10)
 	player_instance.position.z = randi_range(106, 118)
 	player_instance.position.y = 10
 	player_instance.set_multiplayer_authority(data.peer_id)
