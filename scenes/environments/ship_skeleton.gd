2a4b1fbf73b03c4a05d8441c13707d9f8b8ef68c
Kyle Simon <kmsimon1@outlook.com>
2024-12-14 13:00:43 -0500

    some ship skellington work

--- scenes/environments/ship_skeleton.gd
+++ scenes/environments/ship_skeleton.gd
@@ -1,21 +1,21 @@
 extends Area3D
 class_name ShipSkeleton
 
-@export_range(1, 32) var cells_wide: int = 32
-@export_range(1, 64) var cells_long: int = 64
-@export_range(1, 8) var cells_tall: int = 8
+@export_range(1, 32) var cells_wide := 32
+@export_range(1, 64) var cells_long := 64
+@export_range(1, 8) var cells_tall := 8
 
 @export var bridge_position: Vector3 = Vector3(0,0,0)
-@export var bridge_size: Vector3 = Vector3(0,0,0)
+@export var bridge_size := Vector3(0,0,0)
 
-@export var engines_position: Vector3 = Vector3(0,0,0)
-@export var engines_size: Vector3 = Vector3(0,0,0)
+@export var engines_position := Vector3(0,0,0)
+@export var engines_size := Vector3(0,0,0)
 
-@export var oxygen_position: Vector3 = Vector3(0,0,0)
+@export var oxygen_position := Vector3(0,0,0)
 @export var airlock_positions: Dictionary
 
-@export var minimum_timer: float = 1200.0
-@export var maximum_timer: float = 1800.0
+@export var minimum_timer := 1200.0
+@export var maximum_timer := 1800.0
 @onready var bounding_area: CollisionShape3D = %ShipBox
 
 func _ready():
@@ -24,7 +24,28 @@ func _ready():
 	bounding_area.position = Vector3(0, box.size.y * 8, 0)
 	
 func generate():
-	pass
+	var bridge_scene := preload("res://scenes/environments/cells/bridge_test.tscn")
+	var engine_scene := preload("res://scenes/environments/cells/engine_test.tscn")
+	var oxygen_scene := preload("res://scenes/environments/cells/oxygen_test.tscn")
+	var airlock_scene := preload("res://scenes/environments/cells/airlock_test.tscn")
 	
+	var bridge := bridge_scene.instantiate()
+	bridge.position = bridge_position
+	add_child(bridge)
+	
+	var engine := engine_scene.instantiate()
+	engine.position = engines_position
+	add_child(engine)
+	
+	var oxygen := oxygen_scene.instantiate()
+	oxygen.position = oxygen_position
+	add_child(oxygen)
+	
+	for airlock_key in airlock_positions:
+		var airlock_position: Vector3 = airlock_positions[airlock_key]
+		var airlock := airlock_scene.instantiate()
+		airlock.position = airlock_position
+		add_child(airlock)
+	# use PhysicsDirectSpaceState3D to query the physics space before placing randomized rooms
 func _process(_delta):
 	pass
