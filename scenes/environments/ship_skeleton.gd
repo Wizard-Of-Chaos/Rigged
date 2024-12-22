ee34d1c007633d0d1660b4815f4190087d04e2bd
Kyle Simon <kmsimon1@outlook.com>
2024-12-22 18:18:27 -0500

    Proc gen redesign baybeeeee

--- scenes/environments/ship_skeleton.gd
+++ scenes/environments/ship_skeleton.gd
@@ -1,30 +1,26 @@
-extends Area3D
+extends GridMap
 class_name ShipSkeleton
 
-const CELL_WIDTH := 32
-const CELL_HEIGHT := 16
-const CELL_LENGTH := 32
-
 const MAX_ROOM_PLACEMENT_ATTEMPTS := 5
+
 @export_range(1, 32) var cells_wide := 32
 @export_range(1, 64) var cells_long := 64
 @export_range(1, 8) var cells_tall := 8
 
-@export var bridge_position: Vector3 = Vector3(0,0,0)
+@export var bridge_position: Vector3i = Vector3i(0,0,0)
 
-@export var engines_position := Vector3(5 * CELL_WIDTH,0,0)
+@export var engines_position := Vector3i(5,0,0)
 
-@export var oxygen_position := Vector3(0,0,3 * CELL_LENGTH)
+@export var oxygen_position := Vector3i(0,0,3)
 @export var airlock_positions: Dictionary
 
 @export var max_rooms: int
-@export var room_types: Array[PackedScene]
+@export var room_types: Array[String]
 
 @export var rng_seed: int
 
 @export var minimum_timer := 1200.0
 @export var maximum_timer := 1800.0
-@onready var bounding_area: CollisionShape3D = %ShipBox
 
 @export var generate_on_ready := false
 
@@ -37,61 +33,29 @@ func _ready():
 		rng_seed = _rng.seed
 	else:
 		_rng.seed = rng_seed
-	var box: BoxShape3D = bounding_area.shape
-	box.size = Vector3(cells_wide * CELL_WIDTH, cells_tall * CELL_HEIGHT, cells_long * CELL_LENGTH)
-	bounding_area.position = Vector3(0, box.size.y * 8, 0)
 	if generate_on_ready:
 		generate()
 	
 func generate():
-	var bridge_scene := preload("res://scenes/environments/cells/bridge_test.tscn")
-	var engine_scene := preload("res://scenes/environments/cells/engine_test.tscn")
-	var oxygen_scene := preload("res://scenes/environments/cells/oxygen_test.tscn")
-	var airlock_scene := preload("res://scenes/environments/cells/airlock_test.tscn")
+	var bridge_tile := mesh_library.find_item_by_name("bridge")
+	var engine_tile := mesh_library.find_item_by_name("engine")
+	var oxygen_tile := mesh_library.find_item_by_name("oxygen")
+	var airlock_tile := mesh_library.find_item_by_name("airlock")
 	
-	var bridge := bridge_scene.instantiate()
-	bridge.position = bridge_position
-	add_child(bridge)
-	
-	var engine := engine_scene.instantiate()
-	engine.position = engines_position
-	add_child(engine)
-	
-	var oxygen := oxygen_scene.instantiate()
-	oxygen.position = oxygen_position
-	add_child(oxygen)
+	set_cell_item(bridge_position, bridge_tile)
+	set_cell_item(engines_position, engine_tile)
+	set_cell_item(oxygen_position, oxygen_tile)
 	
 	for airlock_key in airlock_positions:
-		var airlock_position: Vector3 = airlock_positions[airlock_key]
-		var airlock := airlock_scene.instantiate()
-		airlock.position = airlock_position
-		add_child(airlock)
-	# use PhysicsDirectSpaceState3D to query the physics space before placing randomized rooms
+		var airlock_position: Vector3i = airlock_positions[airlock_key]
+		set_cell_item(airlock_position, airlock_tile)
 	
 	for i in max_rooms:
 		for attempt in MAX_ROOM_PLACEMENT_ATTEMPTS:
 			var room_type := room_types[_rng.randi_range(0, room_types.size() - 1)]
-			var potential_room: ShipCell = room_type.instantiate()
-			var floor_number := _rng.randi_range(0, cells_tall - 1)
-			var y_coord := floor_number * CELL_HEIGHT
-			var x_coord := _rng.randf_range(0, cells_wide * CELL_WIDTH)
-			var z_coord := _rng.randf_range(0, cells_long * CELL_LENGTH)
-			
-			var shape_query := PhysicsShapeQueryParameters3D.new()
-			shape_query.collide_with_areas = true
-			shape_query.collide_with_bodies = false
-			shape_query.exclude.append(bounding_area.shape.get_rid())
-			# TODO: fine tune the .margin of this query to have rooms have a minimum spacing
-			potential_room.position = Vector3(x_coord, y_coord, z_coord)
-			# TODO: make a box on the physics server to query instead of doing this
-			add_child(potential_room)
-			shape_query.exclude.append(potential_room.bounding_area.shape.get_rid())
-			shape_query.shape_rid = potential_room.bounding_area.shape.get_rid()
-			var physics_space_rid := PhysicsServer3D.area_get_space(get_rid())
-			var physics_space := PhysicsServer3D.space_get_direct_state(physics_space_rid)
-			var collisions := physics_space.intersect_shape(shape_query)
-			
-			if collisions.size() == 0:
-				break
-			remove_child(potential_room)
-	print("hooray ship generated!")
+			var room_tile := mesh_library.find_item_by_name(room_type)
+			var y_coord := _rng.randi_range(0, cells_tall - 1)
+			var x_coord := _rng.randi_range(0, cells_wide)
+			var z_coord := _rng.randi_range(0, cells_long)
+			if get_cell_item(Vector3i(x_coord, y_coord, z_coord)) == INVALID_CELL_ITEM:
+				set_cell_item(Vector3i(x_coord, y_coord, z_coord), room_tile)
