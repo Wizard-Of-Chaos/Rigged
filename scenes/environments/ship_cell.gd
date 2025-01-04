22b5118271d88ad67a59b35d3e99b24ed7a46324
Alexander Wiecking <sandosius@gmail.com>
2025-01-04 16:39:08 -0500

    that should be a const

--- scenes/environments/ship_cell.gd
+++ scenes/environments/ship_cell.gd
@@ -5,7 +5,12 @@ class_name ShipCell
 @export_range(1, 6) var cells_y = 1
 @export_range(1, 6) var cells_z = 1
 @onready var bounding_area: CollisionShape3D = %Box
-@export var door_offsets: PackedVector3Array
+const door_offsets: PackedVector3Array = [ # in order
+	Vector3(12, 0, 16), Vector3(4, 0, 16), Vector3(-4, 0, 16), Vector3(-12, 0, 16),
+	Vector3(-16, 0, 12), Vector3(-16, 0, 4), Vector3(-16, 0, -4), Vector3(-16, 0, -12),
+	Vector3(-12, 0, -16), Vector3(-4, 0, -16), Vector3(4, 0, -16), Vector3(12, 0, -16),
+	Vector3(16, 0, -12), Vector3(16, 0, -4), Vector3(16, 0, 4), Vector3(16, 0, 12),
+]
 
 func _ready():
 	var box: BoxShape3D = bounding_area.shape
