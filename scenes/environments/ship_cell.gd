e8d0bfa1de517d0c8345f39ea5a679c6998009d5
Alexander Wiecking <sandosius@gmail.com>
2025-01-04 16:16:44 -0500

    added door offset array

--- scenes/environments/ship_cell.gd
+++ scenes/environments/ship_cell.gd
@@ -5,10 +5,7 @@ class_name ShipCell
 @export_range(1, 6) var cells_y = 1
 @export_range(1, 6) var cells_z = 1
 @onready var bounding_area: CollisionShape3D = %Box
-#If you put anything other than vectors in this I'll fucking kill you
-@export var entry_positions: Dictionary
-enum ENTRY_SIZE {SMALL, LARGE, VENT}
-@export var entry_position_sizes: Dictionary
+@export var door_offsets: PackedVector3Array
 
 func _ready():
 	var box: BoxShape3D = bounding_area.shape
